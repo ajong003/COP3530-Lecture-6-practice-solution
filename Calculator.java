db10a2b05c0fec530536d4f901c05f5739e31312
ajong003 <ajong003@fiu.edu>
2023-02-06 14:49:12 -0500

    1

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        InfixToPostfixGenerator converter = new InfixToPostfixGenerator();
+
+
+        System.out.println("Input valid postfix expressions for evaluation. Hit ENTER only to quit.");
+        userInput=input.nextLine();
+        try {
+            String postfix =converter.convert(userInput);
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());        }
     }
 }
